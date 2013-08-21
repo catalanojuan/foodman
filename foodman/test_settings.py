c07ac9ad2b0e02363e22479307192def613351f8
Juan Catalano <catalanojuan@gmail.com>
2013-08-21 13:42:56 +0000

    Modified test_settings to use sqlite3.

--- foodman/test_settings.py
+++ foodman/test_settings.py
@@ -24,10 +24,8 @@ if ENV_NAME is 'prod':
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'foodmandb',                      # Or path to database file if using sqlite3.
-            'USER': 'root',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         }
